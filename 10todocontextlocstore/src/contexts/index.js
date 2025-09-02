b4150dbf14fc867f4d3889ad4c57e1f0f5ab07df
Sudhanshu Kandwal <166312263+sudhanshu2207@users.noreply.github.com>
2025-09-02 10:57:33 +0530

    Add files via upload

---
+++ 10todocontextlocstore/src/contexts/index.js
@@ -0,0 +1,7 @@
+import TodoForm from "../components/TodoForm";
+import TodoItem from "../components/TodoItem";
+import { createContext } from "react";
+export {TodoContext, TodoProvider, useTodo} from "./TodoContext"
+
+
+export {TodoForm, TodoItem}
